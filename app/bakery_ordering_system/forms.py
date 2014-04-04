6f8d3459bae77a1e6cce5111916e3f3b6df11fb5
Mitzi Song <mitzisong@mitzi-songs-macbook.local>
2014-04-04 15:38:04 -0700

    css frenzy

--- app/bakery_ordering_system/forms.py
+++ app/bakery_ordering_system/forms.py
@@ -3,17 +3,17 @@ from wtforms import TextField, SelectField, RadioField, SubmitField, validators,
 
  
 class ContactForm(Form):
-    name = TextField("Name",  [validators.Required("Please enter your name.")])
-    email = TextField("Email",  [validators.Required("Please enter your email address.")])
-    subject = TextField("Subject",  [validators.Required("Please enter a subject.")])
-    message = TextAreaField("Message",  [validators.Required("Please enter a message.")])
+    name = TextField("Name*",  [validators.Required("Please enter your name.")])
+    email = TextField("Email*",  [validators.Required("Please enter your email address.")])
+    subject = TextField("Subject*",  [validators.Required("Please enter a subject.")])
+    message = TextAreaField("Message*",  [validators.Required("Please enter a message.")])
     submit = SubmitField("Send")
 
 class SignupForm(Form):
-    firstname = TextField("First name", [validators.Required("Please enter your first name.")])
-    lastname = TextField("Last name", [validators.Required("Please enter your last name.")])
-    phonenumber = TextField("Phone number", [validators.Required("Please enter your phone number.")])
-    email = TextField("Email", [validators.Required("Please enter your email address.")])
+    firstname = TextField("First name*", [validators.Required("Please enter your first name.")])
+    lastname = TextField("Last name*", [validators.Required("Please enter your last name.")])
+    phonenumber = TextField("Phone number*", [validators.Required("Please enter your phone number.")])
+    email = TextField("Email*", [validators.Required("Please enter your email address.")])
     day = SelectField("What day would you like your cupcakes?", choices=[("1", "1"), ("2", "2"), ("3", "3"), ("4", "4"), ("5", "5"), ("6", "6"), ("7", "7"), ("8", "8"), ("9", "9"), ("10", "10"), ("11", "11"), ("12", "12"), ("13", "13"), ("14", "14"), ("15", "15"), ("16", "16"), ("17", "17"), ("18", "18"), ("19", "19"), ("20", "20"), ("21", "21"), ("22", "22"), ("23", "23"), ("24", "24"), ("25", "25"), ("26", "26"), ("27", "27"), ("28", "28"), ("29", "29"), ("30", "30"), ("31", "31")])
     month = SelectField("What month would you like your cupcakes?", choices=[("jan", "January"), ("feb", "February"), ("mar", "March"),  ("apr", "April"), ("may", "May"), ("jun", "June"), ("jul", "July"), ("aug", "August"), ("sep", "September"), ("oct", "October"), ("nov", "November"), ("dec", "December")])
     year = SelectField("What year would you like your cupcakes?", choices=[("2014", "2014"), ("2015", "2015"), ("2016", "2016"), ("2017", "2017"), ("2018", "2018"), ("2019", "2019"), ("2020", "2020"), ("2021", "2021")])
@@ -31,11 +31,11 @@ class DeliveryRecipientForm(Form):
     altname = TextField("Name")
     altphonenumber = TextField("Phone number")
     companyname = TextField("Company Name (if applicable)")
-    streetaddress = TextField("Street Address", [validators.Required("Please enter your street address.")])
+    streetaddress = TextField("Street Address*", [validators.Required("Please enter your street address.")])
     unit = TextField("Unit")
-    city = TextField("City", [validators.Required("Please enter your city.")])
-    state = TextField("State", [validators.Required("Please enter your state.")])
-    zipcode = TextField("Zip Code", [validators.Required("Please enter your zip code.")])
+    city = TextField("City*", [validators.Required("Please enter your city.")])
+    state = TextField("State*", [validators.Required("Please enter your state.")])
+    zipcode = TextField("Zip Code*", [validators.Required("Please enter your zip code.")])
     deliverymethod = RadioField("delivery", choices=[("True", "Pick-Up"), ("False", "Delivery $25")], coerce=unicode)
 
     # def __init__(self, *args, **kwargs):
