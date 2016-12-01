a733fc2c0a93735ccaece3cf88480a81726eb2fc
Mitzi Song <mitzisong@gmail.com>
2016-11-30 21:31:29 -0800

    add host to app.run

--- app/runserver.py
+++ app/runserver.py
@@ -1,4 +1,4 @@
 from bakery_ordering_system import app
 # port = int(os.environ.get('PORT', 5000)) 
 # app.run(host='0.0.0.0', debug=False, port=33507)
-app.listen(process.env.PORT || 5000)
+app.run(host='0.0.0.0', port=port)
